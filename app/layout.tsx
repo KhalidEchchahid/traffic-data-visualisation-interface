c370cff300bf098ef3bf6706fcf050fc4aaee5c4
khalid <echchahid@khalid.com>
2024-08-29 18:07:27 +0200

    set up the facebook pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import WhatsAppButton from "@/components/WhatsAppButton";
 import Head from "next/head";
 import Image from "next/image";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,40 +20,40 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" dir="rtl">
-       <Head>
-        <meta name="facebook-domain-verification" content="5n499kynw54azs1bbeks5i3zwh0g32" />
-         {/* Meta Pixel Code */}
-         <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '785130733698310');
-              fbq('track', 'PageView');
-            `,
-          }}
+      <Head>
+        <meta
+          name="facebook-domain-verification"
+          content="5n499kynw54azs1bbeks5i3zwh0g32"
         />
-        <noscript>
-          <Image
-            height="1"
-            width="1"
-            style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=785130733698310&ev=PageView&noscript=1"
-            alt="facebook-pixel"
-          />
-        </noscript>
-        {/* End Meta Pixel Code */}
       </Head>
       <body className={inter.className}>
         {children}
         <WhatsAppButton />
-        </body>
+
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1014514213384645');
+          fbq('track', 'PageView');
+        `}
+        </Script>
+        <noscript>
+          <Image
+            alt="fb"
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src={`https://www.facebook.com/tr?id=1014514213384645&ev=PageView&noscript=1`}
+          />
+        </noscript>
+      </body>
     </html>
   );
 }
