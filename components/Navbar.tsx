b8d5960d8fdcbb006e7336f6459acc72dc54971c
Khalid <khalid.echchahid@usmba.ac.ma>
2024-12-11 12:37:38 +0100

    new product

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,10 @@
 import React from "react";
-import Link from "next/link";
 
 const Navbar = () => {
   return (
     <div className="bg-gradient-to-r from-yellow-400 to-yellow-600 text-black py-3 px-6 shadow-md flex justify-around items-center">
       <div className="text-lg font-semibold ">
-      التوصيل فقط بـ 20 درهم , و الدفع عند الاستلام! 
+        🎉التوصيل بالمجان , و الدفع عند الاستلام!
       </div>
       {/* <div className="flex items-center space-x-4">
         <Link
