acfcf6e80c6aff659586cdb929caf5c0e2a3b825
Khalid <khalid.echchahid@usmba.ac.ma>
2025-02-09 16:18:31 +0100

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ import Image from "next/image";
 const product = {
   id: 1,
   name: "متوفر لاي نوع من الانشطة",
-  price: 218,
+  price: 199,
   discount: 300,
   images: [
     "/images/12.jpg",
